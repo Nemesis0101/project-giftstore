5061ea118c57474d4e148ab27a73483ede3e2e7a
Ermolovich Artyom <ermolovichartyom@gmail.com>
2020-07-03 01:02:18 +0300

    change view

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-"use strict";var navSlide=function(){var t=document.querySelector(".burger"),n=document.querySelector(".nav__list"),i=document.querySelectorAll(".nav__list li"),e=document.querySelectorAll(".nav__link");t.addEventListener("click",function(){n.classList.toggle("nav__list-active"),i.forEach(function(e,t){e.style.animation?e.style.animation="":e.style.animation="navLinkFade 0.5s ease forwards ".concat(t/7+.3,"s")}),t.classList.toggle("toggle")}),e.forEach(function(e){e.addEventListener("click",function(){n.classList.remove("nav__list-active"),t.classList.remove("toggle"),i.forEach(function(e){e.style.animation=""})})})};navSlide();
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav__list");
+    const navItems = document.querySelectorAll(".nav__list li");
+    const navLinks = document.querySelectorAll(".nav__link");
+
+    burger.addEventListener("click", () => {
+        //toggle nav
+        nav.classList.toggle("nav__list-active");
+
+        //animate links
+        navItems.forEach((item, index) => {
+            if (item.style.animation) {
+                item.style.animation = "";
+            } else {
+                item.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        //burger animation
+        burger.classList.toggle("toggle");
+    });
+
+    navLinks.forEach((item) => {
+        // close menu with link click
+        item.addEventListener("click", () => {
+            nav.classList.remove("nav__list-active");
+            burger.classList.remove("toggle");
+
+            navItems.forEach((item) => {
+                item.style.animation = "";
+            });
+        });
+    });
+};
+
+navSlide();
